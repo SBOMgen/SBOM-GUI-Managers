fa36388259a3880da1fd89f13aaad1607df2d708
Divij Sharma <divijs75@gmail.com>
2023-12-27 23:32:55 +0530

    fix(clean): cleaned unnecessary files

--- client/src/function/authSlice.js
+++ client/src/function/authSlice.js
@@ -1,23 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  isAuthenticated: false,
+    isAuthenticated: false,
 };
 
 export const authSlice = createSlice({
-  name: "auth",
-  initialState,
-  reducers: {
-    toggleAuthentication: (state) => {
-      state.isAuthenticated = !state.isAuthenticated;
+    name: "auth",
+    initialState,
+    reducers: {
+        toggleAuthentication: (state) => {
+            state.isAuthenticated = !state.isAuthenticated;
+        },
+        setTrue: (state, action) => {
+            state.isAuthenticated = true;
+        },
+        setFalse: (state, action) => {
+            state.isAuthenticated = false;
+        }
     },
-    setTrue: (state, action) => {
-      state.isAuthenticated = true;
-    },
-    setFalse: (state, action) => {
-      state.isAuthenticated = false;
-    }
-  },
 });
 export const { toggleAuthentication, setTrue, setFalse } = authSlice.actions;
 export default authSlice.reducer;
